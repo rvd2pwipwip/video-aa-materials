a294611b6d211629daa0f7642a833304e8fd7069
Herve Desrosiers <hdesrosiers@stingray.com>
2020-12-15 08:14:37 -0500

    5. Experiment with Interpolators - done

--- 03-use-value-animators/starter/app/src/main/java/com/raywenderlich/android/menagerie/ui/login/LoginActivity.kt
+++ 03-use-value-animators/starter/app/src/main/java/com/raywenderlich/android/menagerie/ui/login/LoginActivity.kt
@@ -5,6 +5,9 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.BounceInterpolator
+import android.view.animation.DecelerateInterpolator
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.updateLayoutParams
@@ -48,7 +51,10 @@ class LoginActivity : AppCompatActivity(), LoginView {
     val buttonHeight = binding.loginButton.height
 
     val alphaAnimator = ValueAnimator.ofFloat(0f, 1f)
-    alphaAnimator.duration = 1000
+    alphaAnimator.duration = 2000
+//    alphaAnimator.interpolator = DecelerateInterpolator()
+//    alphaAnimator.interpolator = AccelerateDecelerateInterpolator()
+    alphaAnimator.interpolator = BounceInterpolator()
 
     alphaAnimator.addUpdateListener {
       val animatedValue = it.animatedValue as Float
